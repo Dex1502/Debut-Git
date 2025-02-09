387e5f594722849b15e3e7254066e81fae94306a
Dex1502 <Dexter1502@yandex.ru>
2025-02-09 20:15:52 +0300

    1

---
+++ Brand.php
@@ -0,0 +1,18 @@
+<!DOCTYPE >
+<html lang="ru">
+  <head>
+    <title>Дебют - каталог</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/case.css" />
+    <link rel="stylesheet" href="fonts/stylesheet.css" />
+  </head>
+
+  <body>
+    <?php include "include/header.html" ?>
+    <main>
+    </main>
+    <?php include "include/footer.html" ?>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE >
+<html lang="ru">
+  <head>
+    <title>Дебют - каталог</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/index.css" />
+    <link rel="stylesheet" href="fonts/stylesheet.css" />
+  </head>
+
+  <body>
+    <?php include "include/header.html" ?>
+      <div class="Decorate">
+        <let>B</let>
+        <let>U</let>
+      </div>
+    <main>
+      <section class="MainSection">
+        <h1><span class="Sloop">D</span>ebut — мир стиля <br> <span class="Gray">и нового общества</span></h1>
+        <p>Добро пожаловать в Debut — бренд, который меняет восприятие моды и помогает сделать первый шаг в Мир аристократической элегантности. Мы создаем не просто одежду, а философию нового отношения к жизни.</p>
+        <button class="MainButton">В каталог <div><img src="img/ArrowRight.svg"></div></button>
+      </section>
+
+      <section class="PhotosSection">
+        <div class="PhotoContainer-1">
+          <img src="img/Main1.png">
+          <p>Вдохновляемся азиатской культурой. Наш стиль яркий, смелый 
+            и современный. Мы черпаем вдохновение в азиатской моде, которая сочетает оригинальность и утонченность. Это не просто одежда — это искусство, которое позволяет выразить вашу индивидуальность.</p>
+        </div>
+        <div class="PhotoContainer-2">
+          <img src="img/Main2.png">
+          <p>Делаем не просто одежду — это искусство, которое позволяет выразить вашу идею и создать именно тот образ, который вы придумали</p>
+        </div>
+        <div class="Decorate-1">
+        <let>M</let>
+      </div>
+      </section>
+
+      <section class="CatalogSection">
+        <div class="Catalog-Header">
+          <div>
+            <h2><span class="Sloop">K</span>АТАЛОГ</h2>
+            <div>
+              <button class="Active">All</button>
+              <button>WINTER COLLECTION</button>
+              <button>Худи</button>
+              <button>Футболки</button>
+              <button>Брюки</button>
+            </div>
+          </div>
+          <select>
+            <option value="standart">Порядок: по умолчанию</option>
+            <option value="vozrast">Порядок: по возрастанию</option>
+            <option value="ubivan">Порядок: по убыванию</option>
+          </select>
+        </div>
+        <div class="Catalog">
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Купить <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Предзаказ <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Купить <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Предзаказ <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Купить <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Предзаказ <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Купить <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Предзаказ <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Купить <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Предзаказ <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Купить <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+          <div class="Catalog-Card">
+            <div class="FavIcon">
+              <img src="img/CardHeart.svg">
+            </div>
+            <img src="img/Catalog.png">
+            <div>
+              <p><span>Брюки</span><br>6 490 р</p>
+              <button class="MainButton">Предзаказ <div><img src="img/ArrowRight.svg"></div></button>
+            </div>
+          </div>
+        </div>
+        <button id="LoadMore">Загрузить еще</button>
+      </section>
+    </main>
+    <?php include "include/footer.html" ?>
+  </body>
+</html>
